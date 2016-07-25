36229893389b3473c040d35fb262a278d9c00d30
Josh Washke <jwashke@icloud.com>
2016-07-25 10:46:10 -0600

    Write user can login spec

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "links#index"
 
   get "/login", to: "sessions#new", as: :login
+  post "/login", to: "sessions#create", as: :logout
   delete "/logout", to: "sessions#destroy", as: :logout
   resources :users, only: [:new, :create]
 end
---
+++ spec/features/user_can_logout_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.feature "User logs out" do
+  scenario "They are redirected to the login page" do
+
+  end
+end
