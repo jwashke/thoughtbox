2ea8570d0ac985794a37e7f334bbaf6af65b638a
Josh Washke <jwashke@icloud.com>
2016-07-25 10:55:24 -0600

    User can logout spec passing

--- spec/features/user_can_logout_spec.rb
+++ spec/features/user_can_logout_spec.rb
@@ -2,6 +2,21 @@ require "rails_helper"
 
 RSpec.feature "User logs out" do
   scenario "They are redirected to the login page" do
+    user = create(:user, email: "user@example.com")
 
+    visit login_path
+    within ".login-form" do
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "password"
+      click_button "Login"
+    end
+
+    click_link "Logout"
+
+    expect(current_path).to eq(login_path)
+    within ".navbar" do
+      expect(page).to have_content("Login")
+      expect(page).to have_content("Sign Up")
+    end
   end
 end
