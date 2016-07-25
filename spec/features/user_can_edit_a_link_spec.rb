3bf13e54953d7cfdea5ad3ccbdf99f0b2c04c64d
Josh Washke <jwashke@icloud.com>
2016-07-25 13:24:14 -0600

    Write feature test for user can edit a link

--- spec/features/user_can_edit_a_link_spec.rb
+++ spec/features/user_can_edit_a_link_spec.rb
@@ -1,39 +1,44 @@
 require "rails_helper"
 
-RSpec.feature "User can create a link" do
+RSpec.feature "User can edit a link" do
   context "With a valid url and title" do
     scenario "The link is created and they see it on the index" do
       user = create(:user)
+      link = create(:link, user_id: user.id)
+
       ApplicationController.any_instance.stubs(:current_user).returns(user)
 
       visit root_path
+      click_link("Edit Link")
 
-      within (".new-link-form") do
-        fill_in "Name", with: "My Link"
-        fill_in "URL", with: "http://www.mylink.com"
-        click_button "Create Link"
+      within (".edit-link-form") do
+        fill_in "Name", with: "Updated Link"
+        fill_in "URL", with: "http://www.updatedlink.com"
+        click_button "Update Link"
       end
 
       expect(current_path).to eq(root_path)
-      expect(page).to have_content("My Link")
-      expect(page).to have_content("http://www.mylink.com")
+      expect(page).to have_content("Updated Link")
+      expect(page).to have_content("http://www.updatedlink.com")
     end
   end
 
   context "With invalid url" do
-    scenario "The link is not created and they see an error message" do
+    xscenario "The link is not created and they see an error message" do
+      link = create(:link)
       user = create(:user)
       ApplicationController.any_instance.stubs(:current_user).returns(user)
 
       visit root_path
+      click_button("Edit Link")
 
-      within (".new-link-form") do
+      within (".edit-link-form") do
         fill_in "Name", with: "My Link"
         fill_in "URL", with: "mylink!"
-        click_button "Create Link"
+        click_button "Update Link"
       end
 
-      expect(current_path).to eq(root_path)
+      expect(current_path).to eq(edit_link_path)
       expect(page).to have_content("Url is not a valid URL")
       expect(page).not_to have_content("My Link")
       expect(page).not_to have_content("http://www.mylink.com")
@@ -41,18 +46,20 @@ RSpec.feature "User can create a link" do
   end
 
   context "With missing title" do
-    scenario "The link is not created and they see an error message" do
+    xscenario "The link is not created and they see an error message" do
+      link = create(:link)
       user = create(:user)
       ApplicationController.any_instance.stubs(:current_user).returns(user)
 
       visit root_path
+      click_button("Edit Link")
 
-      within (".new-link-form") do
+      within (".edit-link-form") do
         fill_in "URL", with: "http://www.mylink.com"
         click_button "Create Link"
       end
 
-      expect(current_path).to eq(root_path)
+      expect(current_path).to eq(edit_link_path)
       expect(page).to have_content("Name can't be blank")
       expect(page).not_to have_content("My Link")
       expect(page).not_to have_content("http://www.mylink.com")
