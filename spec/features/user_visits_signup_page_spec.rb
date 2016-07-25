fd61aa051c2e645c6b250048193ddae5c0cd420c
Josh Washke <jwashke@icloud.com>
2016-07-25 10:38:26 -0600

    Write spec for password not matching password confirmation

--- spec/features/user_visits_signup_page_spec.rb
+++ spec/features/user_visits_signup_page_spec.rb
@@ -38,4 +38,22 @@ RSpec.feature "User visits sign up page" do
       expect(page).to have_content("Email has already been taken")
     end
   end
+
+  context "With non matching password and confirmation" do
+    scenario "They are redirected back to the sign up page with an error" do
+
+      visit root_path
+      within ".login-form" do
+        click_link "Sign Up"
+      end
+
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password Confirmation", with: "notpassword"
+      click_button "Sign Up"
+
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content("...")
+    end
+  end
 end
