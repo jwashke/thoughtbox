8112dc13abe9229ca96869475166228e7451499f
Josh Washke <jwashke@icloud.com>
2016-07-25 10:24:46 -0600

    Create feature test for user signs up with registered email

--- spec/features/user_visits_signup_page_spec.rb
+++ spec/features/user_visits_signup_page_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.feature "User visits sign up page" do
   context "With valid email and matching passwords" do
     scenario "they are redirected to the links index " do
-
       visit root_path
       within ".login-form" do
         click_link "Sign Up"
@@ -18,6 +17,25 @@ RSpec.feature "User visits sign up page" do
       within ".navbar" do
         expect(page).to have_content("Logout")
       end
+    end
+  end
+
+  context "With an email address already registered" do
+    scenario "They are redirected back to the sign up page with an error" do
+      create(:user, email: "user@example.com")
+
+      visit root_path
+      within ".login-form" do
+        click_link "Sign Up"
+      end
+
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password Confirmation", with: "password"
+      click_button "Sign Up"
+
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content("That email is already taken")
     end
   end
 end
